0a29a743bdc45a7dda616129a9a05e77a6789beb
Nate Orange <cc.lemonhead@gmail.com>
2024-05-16 09:27:17 -0400

    changed to postgres2

--- app/api/avatar/route.ts
+++ app/api/avatar/route.ts
@@ -229,7 +229,7 @@ export async function POST(req: Request) {
   });
 
   const model = new ChatOpenAI({
-    modelName: "gpt-4-turbo",
+    modelName: "gpt-3.5-turbo",
     temperature: 0,
     streaming: true,
     maxTokens: -1,
